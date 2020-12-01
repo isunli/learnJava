45aedbdff889276380f1f22ac90af120c5c87757
isunl <ls@ls.com>
2020-12-01 11:53:08 -0500

    Add hello world

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -3,5 +3,11 @@ package helloworld;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int myFirstNumber = 5;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Maximum Value = "+myMaxIntValue);
+        System.out.println("Integer Minimum Value = "+myMinIntValue);
+        System.out.println(myFirstNumber);
     }
 }
